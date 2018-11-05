2674c6d8da7ff0453b8f23ec22d625970da3ec3a
mdziedzi <marcindziedzic8@gmail.com>
2018-11-05 22:07:41 +0100

    Created client script

---
+++ lista_zgloszen.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl 
+
+use Tk;
+use strict;
+
+my $mw = MainWindow->new;
+$mw->geometry("400x400");
+$mw->title("Zglaszanie problemu");
+ 
+#$mw->Label(-text => 'Hello World')->pack();
+
+$mw->Label(-text => "Strona internetowa: ")->pack();
+my $webPageEntry = $mw->Entry(-background => 'black', -foreground => 'white', -width => 30)->pack();
+
+$mw->Label(-text => "Adres do korespondencji: ")->pack();
+my $addressEntry = $mw->Entry(-background => 'black', -foreground => 'white', -width => 30)->pack();
+
+$mw->Label(-text => "Opis problemu: ")->pack();
+my $descriptionEntry = $mw->Entry(-background => 'black', -foreground => 'white', -width => 60)->pack();
+
+my $sendButton = $mw->Button(-text => "Send", -command =>\&send)->pack();
+
+#$mw->Button(-text => "Close", -command =>sub{exit})->pack();
+
+sub send() {
+	print "*sended*\n";
+	$mw->Label(-text => "Dziekujemy za zgloszenie")->pack();
+	print $webPageEntry -> get();
+	print "\n";
+	print $addressEntry -> get();
+	print "\n";
+	print $descriptionEntry -> get();
+	print "\n";
+	
+	writeToFile();
+}
+
+sub writeToFile() {
+	my $filename = 'zgloszenia.txt';
+	open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+	print $fh "***\n";
+	print $fh $webPageEntry -> get();
+	print $fh "\n";
+	print $fh $addressEntry -> get();
+	print $fh "\n";
+	print $fh $descriptionEntry -> get();
+	print $fh"\n";
+	print $fh"NOWY\n";
+	print $fh "***\n";
+	close $fh;
+	print "done\n";
+}
+
+MainLoop;
